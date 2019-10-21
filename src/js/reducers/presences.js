9178897baeef47dd608cbb9904cfe3b2dd326f83
Jo du Plessis <jo@joduplessis.com>
2019-10-21 22:53:05 +0200

    Added comments

--- src/js/reducers/presences.js
+++ src/js/reducers/presences.js
@@ -8,9 +8,13 @@ const initialState = {
 export default (state = initialState, action) =>
   produce(state, draft => {
     switch (action.type) {
+      // This gets updated from the initialize() function
+      // In the common.js actions file (setInterval)
       case 'UPDATE_USER_PRESENCE':
+        // Do they exist already
         const user = state.users.filter(user => user.userId == action.payload.userId).flatten()
 
+        // If they do - then update their heartbeat
         if (user) {
           draft.users = state.users.map(user => {
             const { userId } = user
@@ -21,6 +25,7 @@ export default (state = initialState, action) =>
             return user
           })
         } else {
+          // If not, then create them
           draft.users.push({ userId: action.payload.userId, heartbeat: new Date() })
         }
         break
