268b837deab911121fa29734aae53a54423e94cb
Jo du Plessis <jo@joduplessis.com>
2019-11-03 13:58:57 +0200

    Added folder clean for production builds

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = env => {
   return {
@@ -27,6 +28,7 @@ module.exports = env => {
       maxAssetSize: 512000
     },
     plugins: [
+      new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, './src/index.html'),
         socketio: 'https://websocket.weekday.sh/socket.io/socket.io.js',
