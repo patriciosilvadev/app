c919c086eb9005bf469999547df152e52b4178a4
Jo du Plessis <jo@joduplessis.com>
2020-01-10 17:18:56 +0200

    New OG service

---
+++ src/js/services/opengraph.service.js
@@ -0,0 +1,17 @@
+import CookiesService from './cookies.service'
+import { API_HOST, JWT } from '../environment'
+
+export default class OpengraphService {
+  static fetchUrl(url) {
+    const token = CookiesService.getCookie(JWT)
+
+    return fetch(`${API_HOST}/opengraph`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      body: JSON.stringify({ url }),
+    })
+  }
+}
