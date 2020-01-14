4f1761372bb04ed8c02e709064de28271480b367
Jo du Plessis <jo@joduplessis.com>
2020-01-14 09:03:53 +0200

    Removed the default constructor

--- src/js/services/upload.service.js
+++ src/js/services/upload.service.js
@@ -2,25 +2,6 @@ import { API_HOST, JWT } from '../environment'
 import CookiesService from './cookies.service'
 
 export default class UploadService {
-  constructor(file, channelId = null, messageId = null) {
-    const form = new FormData()
-    const token = CookiesService.getCookie(JWT)
-
-    // Add our file
-    form.append('file', file)
-    form.append('channelId', channelId)
-    form.append('messageId', messageId)
-
-    // Make the request & return it
-    return fetch(API_HOST + '/upload', {
-      method: 'POST',
-      headers: {
-        Authorization: 'Bearer ' + token,
-      },
-      body: form,
-    })
-  }
-
   static getUploadUrl(filename, mime) {
     const token = CookiesService.getCookie(JWT)
 
