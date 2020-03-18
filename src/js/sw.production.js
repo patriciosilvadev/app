f704df85f899a938d13fc1fbf8fd7e3abd1e7029
Jo du Plessis <jo@joduplessis.com>
2020-03-18 14:57:22 +0200

    Updated from staging

--- src/js/sw.production.js
+++ src/js/sw.production.js
@@ -35,7 +35,7 @@ workbox.routing.registerRoute(new RegExp(/\/localhost/), new workbox.strategies.
 
 // Skip waiting
 addEventListener('message', event => {
-  console.log(`MESSAGE → ${event.data}`)
+  console.log(`PROD:MESSAGE → ${event.data}`)
 
   if (event.data == 'SKIP_WAITING') skipWaiting()
 })
@@ -48,26 +48,23 @@ addEventListener('push', event => {
   }
 
   const title = data.title || 'Yack'
-  const message = data.message || 'This is the default message'
+  const body = data.body || 'This is the default message'
   const icon = 'https://weekday-marketing.s3-us-west-2.amazonaws.com/logo-transparent.png'
+  const image = 'https://weekday-marketing.s3-us-west-2.amazonaws.com/logo-transparent.png'
+  const tag = 'yack-notification'
+
+  self.registration.showNotification(title, {
+    body,
+    tag,
+    icon,
+    image,
+  })
 
   /*
-  var notification = new self.Notification(title, {
-    body: message,
-    tag: 'yack-notification',
-    icon: icon,
-  })
-  */
-
-  var notification = self.registration.showNotification(title, {
-    body: message,
-    tag: 'yack-notification',
-    icon: icon,
-  })
-
   notification.addEventListener('click', function() {
     if (clients.openWindow) {
       clients.openWindow('https://yack.app')
     }
   })
+  */
 })
