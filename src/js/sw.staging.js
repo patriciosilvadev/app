6b5d2fbb644d28b776b6e2266702aa820c436371
Jo du Plessis <jo@joduplessis.com>
2020-03-18 14:55:30 +0200

    Testing PN from server

--- src/js/sw.staging.js
+++ src/js/sw.staging.js
@@ -13,26 +13,23 @@ addEventListener('push', event => {
   }
 
   const title = data.title || 'Yack'
-  const message = data.message || 'This is the default message'
+  const body = data.body || 'This is the default message'
   const icon = 'https://weekday-marketing.s3-us-west-2.amazonaws.com/logo-transparent.png'
+  const image = 'https://weekday-marketing.s3-us-west-2.amazonaws.com/logo-transparent.png'
+  const tag = 'yack-notification'
+
+  self.registration.showNotification(title, {
+    body,
+    tag,
+    icon,
+    image,
+  })
 
   /*
-  var notification = new self.Notification(title, {
-    body: message,
-    tag: 'yack-notification',
-    icon: icon,
-  })
-  */
-
-  var notification = self.registration.showNotification(title, {
-    body: message,
-    tag: 'yack-notification',
-    icon: icon,
-  })
-
   notification.addEventListener('click', function() {
     if (clients.openWindow) {
       clients.openWindow('https://yack.app')
     }
   })
+  */
 })
