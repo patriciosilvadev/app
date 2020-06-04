df2e7399d02c8efa25636d56a47bae7fc013c2df
Jo du Plessis <jo@joduplessis.com>
2020-06-04 20:09:23 +0200

    Commented out old code - temp

--- src/js/sw.production.js
+++ src/js/sw.production.js
@@ -1,3 +1,41 @@
+// Skip waiting
+addEventListener('message', event => {
+  console.log(`DEV:MESSAGE → ${event.data}`)
+
+  if (event.data == 'SKIP_WAITING') skipWaiting()
+})
+
+addEventListener('push', event => {
+  let data = {}
+
+  if (event.data) {
+    data = event.data.json()
+  }
+
+  const title = data.title || 'Yack'
+  const body = data.body || 'This is the default message'
+  const icon = 'https://yack-marketing.s3-us-west-2.amazonaws.com/logo.png'
+  const image = 'https://yack-marketing.s3-us-west-2.amazonaws.com/logo.png'
+  const tag = 'yack-notification'
+
+  self.registration.showNotification(title, {
+    body,
+    tag,
+    icon,
+    image,
+  })
+
+  /*
+  notification.addEventListener('click', function() {
+    if (clients.openWindow) {
+      clients.openWindow('https://yack.app')
+    }
+  })
+  */
+})
+
+/** DISABLING THIS TEMPORARILY TO TEST
+ 
 workbox.precaching.precacheAndRoute(self.__precacheManifest)
 
 // workbox.setConfig({ debug: false })
@@ -59,12 +97,6 @@ addEventListener('push', event => {
     icon,
     image,
   })
-
-  /*
-  notification.addEventListener('click', function() {
-    if (clients.openWindow) {
-      clients.openWindow('https://yack.app')
-    }
-  })
-  */
 })
+
+*/
