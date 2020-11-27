43b946c674c59b9fb45450c0d94dd6829abeb581
Jo du Plessis <jo@joduplessis.com>
2020-11-27 21:40:35 +0100

    Updated drag graphic

--- src/js/extensions/calendar/components/month-day-task/month-day-task.component.js
+++ src/js/extensions/calendar/components/month-day-task/month-day-task.component.js
@@ -9,6 +9,29 @@ import { logger } from '../../../../helpers/util'
 import { WEEKDAY_DRAGGED_TASK_ID } from '../../../../constants'
 import { IconComponent } from '../../../../components/icon.component'
 
+const TASK_IMAGE_SVG = encodeURI(`
+<svg width='115' height='27' viewBox='0 0 235 53' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve' xmlns:serif='http://www.serif.com/' style='fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;'>
+    <g transform='matrix(1,0,0,1,-393.621,-397.641)'>
+        <g transform='matrix(1,0,0,1,358.779,380.024)'>
+            <g transform='matrix(0.948208,0,0,0.748642,-338.393,-280.074)'>
+                <path d='M640.402,405.16C640.402,401.01 637.742,397.641 634.465,397.641L399.558,397.641C396.281,397.641 393.621,401.01 393.621,405.16L393.621,460.583C393.621,464.733 396.281,468.102 399.558,468.102L634.465,468.102C637.742,468.102 640.402,464.733 640.402,460.583L640.402,405.16Z' style='fill:rgb(242,242,242);'/>
+            </g>
+            <g transform='matrix(0.744185,0,0,0.744185,-249.208,-272.1)'>
+                <path d='M405.983,425.789L418.478,438.284L442.616,414.147' style='fill:none;stroke:rgb(195,216,237);stroke-width:10.75px;'/>
+            </g>
+            <g transform='matrix(1,0,0,1,-362.779,-381.024)'>
+                <path d='M471.881,415.536L591.396,415.536' style='fill:none;stroke:rgb(195,216,237);stroke-width:8px;'/>
+            </g>
+            <g transform='matrix(1,0,0,1,-362.779,-381.024)'>
+                <path d='M474.079,434.447L531.735,434.447' style='fill:none;stroke:rgb(195,216,237);stroke-width:8px;'/>
+            </g>
+        </g>
+    </g>
+</svg>
+`)
+let TASK_IMAGE = document.createElement('img')
+TASK_IMAGE.src = 'data:image/svg+xml,' + TASK_IMAGE_SVG
+
 export const MonthDayTaskComponent = ({ id, channelId, displayChannelName, done, channel, title, user, onClick, subtaskCount, parent }) => {
   const dispatch = useDispatch()
 
@@ -31,11 +54,12 @@ export const MonthDayTaskComponent = ({ id, channelId, displayChannelName, done,
 
   const handleDragEnd = e => {}
 
-  const handleDragStart = e => {}
-
-  const handleDrag = e => {
+  const handleDragStart = e => {
+    e.dataTransfer.setDragImage(TASK_IMAGE, 0, 0)
     window[WEEKDAY_DRAGGED_TASK_ID] = id
   }
+
+  const handleDrag = e => {}
 
   return (
     <div draggable id={id} onDrag={handleDrag} onDragStart={handleDragStart} onDragEnd={handleDragEnd} className="month-day-task">
