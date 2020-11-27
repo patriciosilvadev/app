11ae8641be73a846df9336edddf0b31eef80d1ae
Jo du Plessis <jo@joduplessis.com>
2020-11-27 21:38:14 +0100

    Updated drag graphic

--- src/js/extensions/boards/components/card/card.component.js
+++ src/js/extensions/boards/components/card/card.component.js
@@ -12,9 +12,28 @@ import * as moment from 'moment'
 import dayjs from 'dayjs'
 import GraphqlService from '../../../../services/graphql.service'
 
+const CARD_IMAGE_SVG = encodeURI(`
+<svg width='115' height='27' viewBox='0 0 235 53' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve' xmlns:serif='http://www.serif.com/' style='fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;'>
+    <g transform='matrix(1,0,0,1,-393.621,-397.641)'>
+        <g transform='matrix(1,0,0,1,358.779,380.024)'>
+            <g transform='matrix(0.948208,0,0,0.748642,-338.393,-280.074)'>
+                <path d='M640.402,405.16C640.402,401.01 637.742,397.641 634.465,397.641L399.558,397.641C396.281,397.641 393.621,401.01 393.621,405.16L393.621,460.583C393.621,464.733 396.281,468.102 399.558,468.102L634.465,468.102C637.742,468.102 640.402,464.733 640.402,460.583L640.402,405.16Z' style='fill:rgb(242,242,242);'/>
+            </g>
+            <g transform='matrix(0.744185,0,0,0.744185,-249.208,-272.1)'>
+                <path d='M405.983,425.789L418.478,438.284L442.616,414.147' style='fill:none;stroke:rgb(195,216,237);stroke-width:10.75px;'/>
+            </g>
+            <g transform='matrix(1,0,0,1,-362.779,-381.024)'>
+                <path d='M471.881,415.536L591.396,415.536' style='fill:none;stroke:rgb(195,216,237);stroke-width:8px;'/>
+            </g>
+            <g transform='matrix(1,0,0,1,-362.779,-381.024)'>
+                <path d='M474.079,434.447L531.735,434.447' style='fill:none;stroke:rgb(195,216,237);stroke-width:8px;'/>
+            </g>
+        </g>
+    </g>
+</svg>
+`)
 let CARD_IMAGE = document.createElement('img')
-CARD_IMAGE.src =
-  "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='none' d='M0 0h24v24H0z'/%3E%3Cpath d='M3 13h18v8.002c0 .551-.445.998-.993.998H3.993A.995.995 0 0 1 3 21.002V13zM3 2.998C3 2.447 3.445 2 3.993 2h16.014c.548 0 .993.446.993.998V11H3V2.998zM9 5v2h6V5H9zm0 11v2h6v-2H9z' fill='rgba(149,164,166,1)'/%3E%3C/svg%3E"
+CARD_IMAGE.src = 'data:image/svg+xml,' + CARD_IMAGE_SVG
 
 class CardComponent extends React.Component {
   constructor(props) {
