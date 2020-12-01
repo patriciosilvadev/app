22f8c8a412db0716e64fee1b9111edd095e4fa3c
Jo du Plessis <jo@joduplessis.com>
2020-12-01 15:03:35 +0100

    Local file upload support

--- src/js/services/upload.service.js
+++ src/js/services/upload.service.js
@@ -2,6 +2,25 @@ import { API_HOST, JWT } from '../environment'
 import StorageService from './storage.service'
 
 export default class UploadService {
+  static uploadFileLocal(file) {
+    var formData = new FormData()
+    formData.append('file', file)
+
+    return fetch(`${API_HOST}/upload/local`, {
+      method: 'POST',
+      body: formData,
+    })
+  }
+  static uploadFileS3(file) {
+    var formData = new FormData()
+    formData.append('file', file)
+
+    return fetch(`${API_HOST}/upload`, {
+      method: 'POST',
+      body: formData,
+    })
+  }
+
   static getUploadUrl(filename, mime, secured) {
     const token = StorageService.getStorage(JWT)
 
